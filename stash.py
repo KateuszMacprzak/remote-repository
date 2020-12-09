4815eb1b5bb57fd89caa0a40d75d262bbcaff8b5
Mateusz@MSI <matkac98@gmail.com>
2020-12-09 18:51:30 +0100

    Tekst końcowy
    Wykorzystanie podanych informacji. Próba brancha

--- stash.py
+++ stash.py
@@ -2,3 +2,5 @@ name=input("Enter your name: ")
 print("Hello %s !"%name)
 age=input("Enter your age: ")
 place=input("Enter your place of birth: ")
+x=[name, age, place]
+print (f'So, your name is {x[0]}. Actually you are {x[1]} years old. Whats more, you was born in {x[2]}')
